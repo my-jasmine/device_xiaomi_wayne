eae8d9e00281525cbf1e5588079ce991575d00d2
chandra prakash <scp.thedevil@gmail.com>
2020-02-19 14:23:54 +0530

    Wayne:Initial Revenge OS bringup
    Signed-off-by: chandra prakash <scp.thedevil@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,9 +23,9 @@
 
 # inherit MI 6X lineage device config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/carbon_wayne.mk
+    $(LOCAL_DIR)/revengeos_wayne.mk
 
 COMMON_LUNCH_CHOICES := \
-    carbon_wayne-eng \
-    carbon_wayne-userdebug \
-		carbon_wayne-user
+    revengeos_wayne-eng \
+    revengeos_wayne-userdebug \
+		revengeos_wayne-user
